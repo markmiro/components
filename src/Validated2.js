9f9f39463cb5248ddb217242459edb08d1c6f0ac
Mark Miro <contact@markmiro.com>
2017-12-26 22:12:45 -0800

    Make validate all work with promises

--- src/Validated2.js
+++ src/Validated2.js
@@ -1,4 +1,4 @@
-import { isFunction, mapValues, castArray } from "lodash";
+import { isFunction, mapValues, castArray, flatten, values } from "lodash";
 
 export const normalizeValidations = validations =>
   isFunction(validations) ? validations : () => validations;
@@ -13,6 +13,33 @@ export const validateAll = (validations, formFields) => {
     const toValidate = formFields[fieldKey];
     const messages = validationArray.map(validation => validation(toValidate));
     return messages;
+  });
+};
+
+export const validateAllWithPromises = (validations, formFields) => {
+  // Flatten the validations object so we can run `Promise.all`.
+  // Once they all resolve then we map the array back to the original
+  // shape so the consumer can parse it.
+  const supposedlyValidated = validateAll(validations, formFields);
+  const flattened = flatten(values(supposedlyValidated));
+  const flattenedWithSafety = flattened.map(maybePromise => {
+    if (maybePromise instanceof Promise) {
+      return maybePromise.catch(error => {
+        if (error instanceof Error) {
+          throw error;
+        }
+        return error;
+      });
+    }
+    return maybePromise;
+  });
+  return Promise.all(flattenedWithSafety).then(flattenedResults => {
+    let i = 0;
+    return mapValues(supposedlyValidated, resultArray =>
+      resultArray.map(() => {
+        return flattenedResults[i++];
+      })
+    );
   });
 };
 
--- src/Validated2.test.js
+++ src/Validated2.test.js
@@ -1,6 +1,13 @@
 import validations from "./validations2";
 import { pick } from "lodash";
-import { normalizeValidations, validate, validateAll } from "./Validated2";
+import {
+  normalizeValidations,
+  validate,
+  validateAll,
+  validateAllWithPromises
+} from "./Validated2";
+
+// TODO: add tests to make sure exceptions are handled properly
 
 test("normalizeValidations()", () => {
   const normalized = normalizeValidations(validations);
@@ -42,6 +49,80 @@ describe("validateAll()", () => {
       email: ["", ""],
       name: ["At least 2 characters required"]
     });
+  });
+});
+
+describe("validateAllWithPromises()", () => {
+  test("without promises", () => {
+    const messagesPromise = validateAllWithPromises(
+      {
+        username: [validations.required],
+        email: [validations.required]
+      },
+      {
+        username: "bla",
+        email: "bla@example.com"
+      }
+    );
+    return messagesPromise.then(({ username, email }) => {
+      expect(username).toEqual([""]);
+      expect(email).toEqual([""]);
+    });
+  });
+  test("with promises", () => {
+    const formValidations = {
+      username: [value => Promise.resolve(validations.required(value))],
+      email: [value => Promise.resolve(validations.required(value))]
+    };
+    const formFields = {
+      username: "bla",
+      email: "bla@example.com"
+    };
+    const messagesPromise = validateAllWithPromises(
+      formValidations,
+      formFields
+    );
+    return messagesPromise.then(result => {
+      expect(result).toEqual({ username: [""], email: [""] });
+    });
+  });
+  test("with promises that resolve error messages", () => {
+    const formValidations = {
+      username: [value => Promise.resolve(validations.required(value))],
+      email: [value => Promise.resolve(validations.required(value))]
+    };
+    const formFields = {
+      username: "",
+      email: ""
+    };
+    const messagesPromise = validateAllWithPromises(
+      formValidations,
+      formFields
+    );
+    return messagesPromise.then(result => {
+      expect(result).toEqual({ username: ["Required"], email: ["Required"] });
+    });
+  });
+  test("with promises that reject", () => {
+    const formValidations = {
+      username: [value => Promise.reject(validations.required(value))],
+      email: [value => Promise.reject(validations.required(value))]
+    };
+    const formFields = {
+      username: "",
+      email: ""
+    };
+    const messagesPromise = validateAllWithPromises(
+      formValidations,
+      formFields
+    );
+    return messagesPromise
+      .then(result => {
+        expect(result).toEqual({ username: ["Required"], email: ["Required"] });
+      })
+      .catch(errors => {
+        expect(errors).toEqual("");
+      });
   });
 });
 
