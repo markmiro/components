2fcb88efe6f587dbfbaaffc584d9eebc4682fc5e
Mark Mironyuk <contact@markmiro.com>
2018-01-02 22:29:09 -0800

    Don't make label red if error since it draws too much attention

--- src/ValidatedInput.js
+++ src/ValidatedInput.js
@@ -2,29 +2,27 @@ import React from "react";
 import { Input, Label, InputMessage } from "./FormComponents";
 
 const ValidatedInput = ({
-  label,
-  errorMessage,
-  placeholder,
-  helper,
-  ...props
+    label,
+    errorMessage,
+    placeholder,
+    helper,
+    ...props
 }) => {
-  const finalErrorMessage = helper ? helper.validationMessage : errorMessage;
-  const finalProps = helper ? helper.getProps({ ...props }) : props;
-  return (
-    <div>
-      {!placeholder && (
-        <Label status={finalErrorMessage && "error"}>{label}</Label>
-      )}
-      <Input
-        status={finalErrorMessage && "error"}
-        placeholder={placeholder}
-        {...finalProps}
-      />
-      {finalErrorMessage && (
-        <InputMessage status="error">{finalErrorMessage}</InputMessage>
-      )}
-    </div>
-  );
+    const finalErrorMessage = helper ? helper.validationMessage : errorMessage;
+    const finalProps = helper ? helper.getProps({ ...props }) : props;
+    return (
+        <div>
+            {!placeholder && <Label>{label}</Label>}
+            <Input
+                status={finalErrorMessage && "error"}
+                placeholder={placeholder}
+                {...finalProps}
+            />
+            {finalErrorMessage && (
+                <InputMessage status="error">{finalErrorMessage}</InputMessage>
+            )}
+        </div>
+    );
 };
 
 export default ValidatedInput;
