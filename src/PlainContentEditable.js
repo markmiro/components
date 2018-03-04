67585d9e4fade15ace8b922e272e1c9c1ce3c920
Mark Mironyuk <contact@markmiro.com>
2018-03-03 22:35:33 -0800

    Better handle contenteditable paste
    We used to lose undo history after pasting

--- src/PlainContentEditable.js
+++ src/PlainContentEditable.js
@@ -50,55 +50,16 @@ export default class PlainContentEditable extends Component {
   handleInputKeyDown = e => {
     const editorEvent = toEditorEvent(e);
     console.log("Key combo", editorEvent.comboString);
-    // TODO: paste only plain text
     if (editorEvent.isBlockedKeyCombo() || e.key === "Enter") {
       console.log("prevent");
       e.preventDefault();
     }
   };
   handlePaste = e => {
-    // TODO: we lose undo history because we do paste ourselves
-    // Don't let browser handle paste (it will try to paste HTML some)
+    // https://stackoverflow.com/a/12028136
     e.preventDefault();
-    console.log("paste");
-    console.log(e.clipboardData.getData("text"));
-    console.log(e.target);
-    // https://stackoverflow.com/a/3976125
-    function getCaretPosition(editableDiv) {
-      let caretPos = { start: 0, end: 0 };
-      let sel;
-      let range;
-      if (window.getSelection) {
-        sel = window.getSelection();
-        if (sel.rangeCount) {
-          range = sel.getRangeAt(0);
-          if (range.commonAncestorContainer.parentNode == editableDiv) {
-            caretPos = {
-              start: range.startOffset,
-              end: range.endOffset
-            };
-          }
-        }
-      }
-      return caretPos;
-    }
-
-    console.log(getCaretPosition(e.target));
-    const range = getCaretPosition(e.target);
-    const clipboardText = e.clipboardData.getData("text");
-    const before = this.inputEl.textContent.slice(0, range.start);
-    const after = this.inputEl.textContent.slice(range.end);
-    console.log(before, clipboardText, after);
-    this.inputEl.textContent = before + clipboardText + after;
-
-    // Move cursor to end of pasted content
-    const inputElTextNode = this.inputEl.childNodes[0];
-    const selectRange = document.createRange();
-    selectRange.setStart(inputElTextNode, range.start + clipboardText.length);
-    selectRange.collapse();
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(selectRange);
+    const pasteText = e.clipboardData.getData("text/plain");
+    document.execCommand("insertHTML", false, pasteText);
   };
 
   render() {
