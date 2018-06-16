1d55673a0a78d65afaf11b8cef4f40154e6f40c8
Mark Mironyuk <contact@markmiro.com>
2018-06-16 14:04:09 -0700

    Small refactor for test setup

--- src/validation/Validated.test.js
+++ src/validation/Validated.test.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Validated from "./Validated";
 import { shallow, mount } from "enzyme";
-import jsdom from "jsdom";
-
-const doc = jsdom.jsdom("<!doctype html><html><body></body></html>");
-global.document = doc;
-global.window = doc.defaultView;
 
 describe.only("<Validated />", () => {
   it("renders without crashing", () => {
