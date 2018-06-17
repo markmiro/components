6ea03162f22ed6f1020b9b38e1a7b139fdda9d12
Mark Mironyuk <contact@markmiro.com>
2018-06-16 23:34:42 -0700

    Add storybook examples for different ref situations

--- src/validation/stories/validation-stories.js
+++ src/validation/stories/validation-stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
-import { PageCard } from "../../FormComponents";
+import { PageCard, Input } from "../../FormComponents";
 import AnimatedRegisterForm from "./AnimatedRegisterForm";
 import EmailFieldValidationWithoutLibrary from "./EmailFieldValidationWithoutLibrary";
 import EmailFieldValidationHoc from "./EmailFieldValidationHoc";
@@ -9,6 +9,7 @@ import WithPretendBackend from "./WithPretendBackend";
 import RegisterForm from "./RegisterForm";
 import LoginForm from "./LoginForm";
 import RadioOptions from "./RadioOptions";
+import Validated from "../Validated";
 
 storiesOf("Forms", module)
   .addDecorator(story => <PageCard>{story()}</PageCard>)
@@ -20,4 +21,42 @@ storiesOf("Forms", module)
   .add("WithPretendBackend", () => <WithPretendBackend />)
   .add("RegisterForm", () => <RegisterForm />)
   .add("LoginForm", () => <LoginForm />)
-  .add("RadioOptions", () => <RadioOptions />);
+  .add("RadioOptions", () => <RadioOptions />)
+  .add("refs on <input />", () => (
+    <div>
+      <Validated
+        validations={{
+          bla: () => "Error"
+        }}
+        render={({ bla }, { validateAll }) => (
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              validateAll();
+            }}
+          >
+            {bla.watch(<input />)}
+            {bla.validationMessage}
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      />
+      <Validated
+        validations={{
+          bla: () => "Error"
+        }}
+        render={({ bla }, { validateAll }) => (
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              validateAll();
+            }}
+          >
+            {bla.watch(<Input />)}
+            {bla.validationMessage}
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      />
+    </div>
+  ));
